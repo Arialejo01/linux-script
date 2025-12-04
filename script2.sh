21eb4d75b744c1b8ff93e82714307e8e7071592c
Arialejo01 <arvelascogu@uide.edu.ec>
2025-12-04 13:20:36 +0000

    mi segundo script

---
+++ script2.sh
@@ -0,0 +1,10 @@
+#uname en Linux imprime información sobre el sistema operativo y el hardware
+#uname -a muestra todos los detalles disponibles sobre el sistema
+uname -a
+#lscpu muestra información detallada y legible sobre la arquitectura y el estado de la CPU del sistema
+lscpu
+#free en Linux muestra la cantidad de memoria RAM física y de intercambio (swap) que está usada, libre y disponible en el sistema
+free -h
+#df muestra información sobre el espacio libre en los sistemas de archivos montados en la máquina
+#La opción -h (human-readable) muestra los tamaños en un formato más fácil de leer, como Gigabytes (GB) y Megabytes (MB)
+df -h
